0e266623302c8fbbdecc7e42d36109fe7d1f2b8a
mervegk <merve.keles@orgino.com.tr>
2024-08-11 21:20:50 +0300

    state form operations 2

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,34 @@
 import React, { useState } from 'react'
 
 export default function Form() {
-  const [name, setName] = useState("Murtaza");
-  const [surname, setSurname] = useState("Yeter");
-  const [gender, setGender] = useState("Kadın");
+
+  const [form, setForm] = useState({ name: "", surname: "", gender: "Kadın" })
+
+  const handleChange = (e) => {
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+
   return (
     <div>
       <div>
         İsim
-        <input type="text" placeholder='isim' value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" name='name' placeholder='isim' value={form.name} onChange={handleChange} />
       </div>
       <div>
         Soyisim
-        <input type="text" placeholder='soyisim' value={surname} onChange={(e) => setSurname(e.target.value)} />
+        <input type="text" name='surname' placeholder='soyisim' value={form.surname} onChange={handleChange} />
       </div>
       <div>
-        <select name="" id="" value={gender} onChange={(e) => setGender(e.target.value)}>
+        <select name="gender" id="" value={form.gender} onChange={handleChange}>
           <option value="Kadın">Kadın</option>
           <option value="Erkek">Erkek</option>
         </select>
       </div>
       <div>
         <hr />
-        <p>İsim: {name}</p>
-        <p>Soyisim: {surname}</p>
-        <p>Cinsiyet: {gender}</p>
+        <p>İsim: {form.name}</p>
+        <p>Soyisim: {form.surname}</p>
+        <p>Cinsiyet: {form.gender}</p>
       </div>
     </div>
   )
