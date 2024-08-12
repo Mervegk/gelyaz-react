6e688b6f609b64903d70d6f928a5f7e52bd1ede7
mervegk <merve.keles@orgino.com.tr>
2024-08-13 00:41:51 +0300

    async await

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+//import { useState } from 'react';
 import './App.css';
 import Users from './components/Users';
 /* import Counter from './components/Counter';
--- src/components/Users.js
+++ src/components/Users.js
@@ -3,22 +3,50 @@ import axios from 'axios';
 
 export default function Users() {
   const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
+  /* useEffect(() => {
     axios("https://jsonplaceholder.typicode.com/users")
-      .then(res => setUsers(res.data))
+      .then(res => {
+        setUsers(res.data);
+
+        axios(`https://jsonplaceholder.typicode.com/posts?userId=${res.data[0].id}`)
+          .then(res => setPosts(res.data))
+      })
       .finally(() => setLoading(false))
+  }, []) */
+
+  useEffect(() => {
+    getData()
   }, [])
+
+  const getData = async () => {
+    const { data: users } = await axios("https://jsonplaceholder.typicode.com/users");
+    const { data: posts } = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${users[0].id}`);
+
+    setLoading(false)
+    setUsers(users);
+    setPosts(posts);
+  }
+
   return (
     <div>
       <h1>Users</h1>
       {
-        loading && <h4>Yükleniyor</h4>
+        loading && <h4>Yükleniyor...</h4>
       }
       <ul>
         {
-          users.map(i => <li key={i.id}>{i.name}</li>)
+          users.map(user => <li key={user.id}>{user.name}</li>)
+        }
+      </ul>
+      <ul>
+        {
+          posts.map(post => <li key={post.id}>
+            <p>{post.title}</p>
+            <p>{post.body}</p>
+          </li>)
         }
       </ul>
     </div>
