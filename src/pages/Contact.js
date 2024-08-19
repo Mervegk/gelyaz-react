74d3039733627127a2cd43a48c3d35a5c356c2bd
mervegk <merve.keles@orgino.com.tr>
2024-08-19 22:15:58 +0300

    useFormik hook

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,41 +1,47 @@
 import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useFormik } from 'formik';
 
 export default function Contact() {
+
+  const { handleSubmit, handleChange } = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      message: ''
+    },
+    onSubmit: values => {
+      console.log(values);
+
+    },
+  });
+
   return (
-    <div>
+    <div className='form-container'>
       <h1>İletişim</h1>
-      <Formik
-        initialValues={{ email: '', firstName: '', lastName: '' }}
-        validate={values => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
-        }}
-        onSubmit={(values) => {
-          console.log(values)
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <Field type="text" name="firstName" />
-            <ErrorMessage name="firstName" component="div" />
-            <Field type="text" name="lastName" />
-            <ErrorMessage name="lastName" component="div" />
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
+
+      <form onSubmit={handleSubmit}>
+        <div className='form-input-container'>
+          <div className='form-input-area'>
+            <label htmlFor='Name'> Name</label>
+            <input id='name' name='name' placeholder='Name'
+              onChange={handleChange}
+              className='form-input' />
+          </div>
+          <div className='form-input-area'>
+            <label htmlFor='email'>Email</label>
+            <input id='email' name='email' placeholder='Email'
+              onChange={handleChange}
+              className='form-input' />
+          </div>
+          <div className='form-input-area'>
+            <label htmlFor='message'>Message</label>
+            <textarea id='message' name='message' placeholder='Message'
+              onChange={handleChange}
+              className='form-input' />
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
