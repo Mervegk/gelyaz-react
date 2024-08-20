408c7eda07c1c9e6db798df74a82814068958526
mervegk <merve.keles@orgino.com.tr>
2024-08-20 18:56:45 +0300

    isSubmitting and form reset

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,44 +3,49 @@ import { useFormik } from 'formik';
 
 export default function Contact() {
 
-  const { handleSubmit, handleChange } = useFormik({
+  const { handleSubmit, handleChange, values, isSubmitting } = useFormik({
     initialValues: {
       name: '',
       email: '',
       message: ''
     },
-    onSubmit: values => {
+    onSubmit: async (values, bag) => {
+      await new Promise(r => setTimeout(r, 1000))
       console.log(values);
-
+      bag.resetForm();
     },
   });
 
   return (
     <div className='form-container'>
       <h1>İletişim</h1>
-
       <form onSubmit={handleSubmit}>
-        <div className='form-input-container'>
-          <div className='form-input-area'>
-            <label htmlFor='Name'> Name</label>
-            <input id='name' name='name' placeholder='Name'
-              onChange={handleChange}
-              className='form-input' />
+        <fieldset disabled={isSubmitting} className='form-fieldset'>
+          <div className='form-input-container'>
+            <div className='form-input-area'>
+              <label htmlFor='Name'> Name</label>
+              <input id='name' name='name' placeholder='Name'
+                onChange={handleChange}
+                value={values.name}
+                className='form-input' />
+            </div>
+            <div className='form-input-area'>
+              <label htmlFor='email'>Email</label>
+              <input id='email' name='email' placeholder='Email'
+                onChange={handleChange}
+                value={values.email}
+                className='form-input' />
+            </div>
+            <div className='form-input-area'>
+              <label htmlFor='message'>Message</label>
+              <textarea id='message' name='message' placeholder='Message'
+                onChange={handleChange}
+                value={values.message}
+                className='form-input' />
+            </div>
           </div>
-          <div className='form-input-area'>
-            <label htmlFor='email'>Email</label>
-            <input id='email' name='email' placeholder='Email'
-              onChange={handleChange}
-              className='form-input' />
-          </div>
-          <div className='form-input-area'>
-            <label htmlFor='message'>Message</label>
-            <textarea id='message' name='message' placeholder='Message'
-              onChange={handleChange}
-              className='form-input' />
-          </div>
-        </div>
-        <button type="submit">Submit</button>
+          <button type="submit">Submit</button>
+        </fieldset>
       </form>
     </div>
   )
